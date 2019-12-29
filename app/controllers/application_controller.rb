71788c8bad1c5805e236fab27b7c04e5d18b019b
TD <denningta@gmail.com>
2019-12-28 17:32:54 -0700

    complete authentication system

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,10 +12,16 @@ class ApplicationController < ActionController::Base
   end
 
   def require_user
-      if !logged_in?
-        flash[:danger] = "You must be logged in to perform that action"
-        redirect_to root_path
-      end
+    if !logged_in?
+      flash[:danger] = "You must be logged in to perform that action"
+      redirect_to root_path
+    end
+  end
 
+  def require_same_user
+    if current_user != @article.user
+      flash[:danger] = "You can only edit or delete your own article"
+      redirect_to root_path
+    end
   end
 end
